44ff6039006b61371eb7f9b5d6b2f32c12d75f92
Andrew Cook <55568268+Cook-Andrew@users.noreply.github.com>
2024-04-25 00:38:36 -0700

    initial publish

---
+++ firebase.js
@@ -0,0 +1,64 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDScVEwsrbyS_HpjpMlMS8HQVTur55ftt8",
+    authDomain: "homemanager-8dd42.firebaseapp.com",
+    projectId: "homemanager-8dd42",
+    storageBucket: "homemanager-8dd42.appspot.com",
+    messagingSenderId: "448149708795",
+    appId: "1:448149708795:web:88d98aa8dc5730522ab55e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Get a reference to the auth service
+var auth = getAuth(app);
+
+// Get a reference to the firestore service
+var db = getFirestore(app);
+
+function login() {
+    var email = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    
+    if (localStorage.getItem("email") != null) {
+        email = localStorage.getItem("email");
+    }
+    if (localStorage.getItem("password") != null) {
+        password = localStorage.getItem("password");
+    }
+
+    // Sign in
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            var user = userCredential.user;
+            localStorage.setItem("success", true);
+            localStorage.setItem("email", email);
+            localStorage.setItem("password", password);
+            $(".login").slideToggle();
+            $(".warning").html("");
+
+            // Get data from Firestore
+            getDocs(collection(db, "billslist")).then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    document.getElementById('bills_database_content').innerHTML += "Bill: " + doc.data().bill + "</br>Due: " + doc.data().due + "</br>Ammount: " + doc.data().ammount + '</br></br>';
+                });
+            });
+        })
+        .catch((error) => {
+            console.error('Error signing in: ', error);
+        });
+}
+
+document.getElementById('login').addEventListener('click', login);
+$(function() { 
+    if (localStorage.getItem("success")) {
+        login();
+    }
+});
---
+++ script.js
@@ -0,0 +1,40 @@
+$(function() {
+    //variables
+    console.log( "ready!" );
+    var open_content = "#home_content";
+    var menu_array = [
+        '<li class="menu_item" id="meals_menu"> Meal Planning </li>',
+        '<li class="menu_item" id="bills_menu"> Bill Manager </li>',
+        '<li class="menu_item" id="passwords_menu"> Encrypted Password Manager </li>',
+        '<li class="menu_item" id="dev_menu"> Web Development Resources </li>',
+        '<li class="menu_item" id="splatoon_menu"> Splatoon Resources </li>'
+    ];
+    var success = false;
+    if (localStorage.getItem("success") != null) {
+        success = localStorage.getItem("success");
+    }
+
+    //setup functions
+    $("#home_content").slideToggle();
+    menu_array.forEach(element => {
+        $("#top_menu").append(element+"<span> | </span>");
+        $("#bottom_menu").append(element);
+    });
+
+    // event functions
+    /*$("#login").on( "click", function() {
+        if (success) {
+            $(".login").slideToggle();
+        }
+    });*/
+
+    $(".menu_item").on("click", function() {
+        var id = $(this).attr('id');
+        id = id.split('_');
+        var new_content = "#"+id[0]+"_content";
+        $(open_content).slideToggle();
+        $(new_content).slideToggle();
+        open_content = new_content;
+        $('html, body').animate({ scrollTop: $("main").offset().top - 100 }, 500);
+    });
+});
